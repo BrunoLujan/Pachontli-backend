04aca247655a17167e1719ddfae0db09762fed29
Luis Fajardo <lrfs_13@hotmail.com>
2021-01-09 20:51:07 -0600

    migracion de cliente y mascota

---
+++ database/migrations/2021_01_10_021234_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->string('telefono', 10);
+            $table->timestamps();
+
+            $table->foreign('idUsuario')->references('id')->on('usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
---
+++ database/migrations/2021_01_10_022736_create_mascotas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMascotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mascotas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+            $table->string('nombre');
+            $table->string('sexo');
+            $table->string('especie');
+            $table->string('raza');
+            $table->string('peso');
+            $table->string('altura');
+            $table->integer('edad');
+            $table->string('descripcion');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mascotas');
+    }
+}
