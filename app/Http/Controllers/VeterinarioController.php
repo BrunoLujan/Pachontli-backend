80a20543401e1c534116955052edb52adf3825ad
Luis Fajardo <lrfs_13@hotmail.com>
2021-01-11 14:57:30 -0600

    correcion
    sadasd

--- app/Http/Controllers/VeterinarioController.php
+++ app/Http/Controllers/VeterinarioController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Veterinario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use JWTAuth;
 
 class VeterinarioController extends Controller
 {
